e692af3ca81a469f2bc00850336c14679ec9f2f4
Shuja-ud-din <shuja1339@gmail.com>
2025-04-23 21:16:51 +0500

    chore: fixed redis error of bullmq

--- src/modules/queue/queue.module.ts
+++ src/modules/queue/queue.module.ts
@@ -1,11 +1,22 @@
 import { BullModule } from '@nestjs/bullmq';
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { QUEUE_NAME } from 'src/common/constants/constants';
 
 import { QueueService } from './queue.service';
 
 @Module({
   imports: [
+    ConfigModule,
+    BullModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        connection: {
+          url: configService.get<string>('redis.url'),
+        },
+      }),
+    }),
     BullModule.registerQueue({
       name: QUEUE_NAME,
     }),
